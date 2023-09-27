ef21e3a4c0c64eb24bba9ec57d929f21259b92b1
Salman Younas <126088691+syproo@users.noreply.github.com>
2023-09-27 23:20:19 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL);
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    });
     console.log(`Connected to Database, ${connection.connection.host}`);
   } catch (error) {
     console.log(`Error in Database ${error}`);
